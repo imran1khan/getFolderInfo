92cfc9213385de96ed6f14a911f1500c62f6715f
imran1khan <107169527+imran1khan@users.noreply.github.com>
2024-05-29 21:11:34 +0530

    commit

--- src/howToUse.ts
+++ src/howToUse.ts
@@ -1,7 +1,7 @@
 import { makeJsonFile, seeFiles2 } from "./getFiles";
 const testFolderPath = `D:\\VS_code\\youtubeProject2\\eraser_clone\\eraser_clone-master`;
 const outputFolderPath=`D:\\VS_code\\youtubeProject2\\OpenAi\\mainProject\\output`;
-// const tstFiles=`D:\\VS_code\\youtubeProject2\\OpenAi\\mainProject\\tsconfig.json`;
+const tstFiles=`D:\\VS_code\\youtubeProject2\\OpenAi\\mainProject\\tsconfig.json`;
 const files = seeFiles2(testFolderPath,['README.md','favicon.ico','node_modules','dist','package-lock.json','.gitignore','tsconfig.json','tailwind.config.ts','public'])
 makeJsonFile(files,outputFolderPath);
 export {makeJsonFile,seeFiles2}
